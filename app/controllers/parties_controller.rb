715070e3de393c6961a802b3be55c89ab3abfcae
PJ Murray <pmurray@godaddy.com>
2015-09-22 13:55:13 -0700

    party time

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,8 +1,11 @@
 class PartiesController < ApplicationController
   def show
     if request.subdomain.present?
+      # @party_person = PartyPerson.find_by_subdomain()
+      @party_person = OpenStruct.new(name: request.subdomain)
       render 'show'
     else
       render 'index'
+    end
   end
 end
